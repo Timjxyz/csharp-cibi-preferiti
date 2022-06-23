8cd58bdb92abbf6dcbd3660c39fbce0c2048a5ee
Timothy Gaither <timjgaither@gmail.com>
2022-06-23 16:56:18 +0200

    push esercizio

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-String[] piatti = { "Carbonara", "Braciola", "Pollo allo spiedo", "Insalata", "Hamburger", "Risotto" };
+String[] piatti = { "Carbonara", "Braciola", "Pollo allo spiedo", "Insalata", "Hamburger", "Risotto", "Pasticcio"};
 
 //Stampo la lunghezza della classifica
 Console.WriteLine("La classifica dei miei piatti preferiti è composta da " + piatti.Length + " elementi");
@@ -17,5 +17,24 @@ Console.WriteLine("Il mio piatto preferito è " + piatti[0]);
 //Stampo il piatto che preferisco di meno
 string last = piatti[piatti.Length - 1];
 Console.WriteLine("Il piatto che preferisco di meno " + last);
+
+//Trovo l'elemento centrale se l'array è pari
+    string mid = piatti[piatti.Length / 2];
+
+//Prendo i piatti centrali 
+    string oddmid= piatti[piatti.Length / 2 + 1];
+if (piatti.Length % 2==0)
+{
+    Console.WriteLine("Il piatto che sta a metà è  " + mid);
+}
+else
+{
+    Console.WriteLine ("I piatti che stanno a metà sono " + mid + " e " + oddmid);
+    
+}
+
+
+
+
 
 
